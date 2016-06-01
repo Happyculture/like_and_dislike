cbdff9bd93e4cb7ddbf976181a50af6947eb81af
Edouard Cunibil <admin@duael.fr>
2016-06-01 16:51:10 +0200

    Use like_dislike.helper:vote() in the VoteController.

--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -9,8 +9,9 @@ namespace Drupal\like_and_dislike\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\votingapi\Entity\Vote;
-use Drupal\votingapi\Entity\VoteType;
+use Drupal\like_and_dislike\AlreadyVotedException;
+use Drupal\like_and_dislike\LikeDislikeHelperInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -19,40 +20,47 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class VoteController extends ControllerBase implements ContainerInjectionInterface {
 
-  protected $request;
+  /**
+   * @var \Drupal\like_and_dislike\LikeDislikeHelperInterface
+   */
+  protected $likeDislikeHelper;
+
+  /**
+   * VoteController constructor.
+   *
+   * @param \Drupal\like_and_dislike\LikeDislikeHelperInterface $likeDislikeHelper
+   */
+  public function __construct(LikeDislikeHelperInterface $likeDislikeHelper) {
+    $this->likeDislikeHelper = $likeDislikeHelper;
+  }
 
   /**
    * {@inheritdoc}
    */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('like_dislike.helper')
+    );
+  }
+
+  /**
+   * Callback for like_and_dislike.vote route.
+   */
   function vote($entity_type_id, $vote_type_id, $entity_id, Request $request) {
 
-    $vote_storage = \Drupal::entityManager()->getStorage('vote');
-    $user_votes = $vote_storage->getUserVotes(
-      \Drupal::currentUser()->id(),
-      $vote_type_id,
-      $entity_type_id,
-      $entity_id
-    );
-
-    if (empty($user_votes)) {
-      $vote_type = VoteType::load($vote_type_id);
-      $vote = Vote::create(['type' => $vote_type_id]);
-      $vote->setVotedEntityId($entity_id);
-      $vote->setVotedEntityType($entity_type_id);
-      $vote->setValueType($vote_type->getValueType());
-      $vote->setValue(1);
-      $vote->save();
-
+    try {
+      $this->likeDislikeHelper->vote($entity_type_id, $entity_id, $vote_type_id);
       drupal_set_message(t('Your :type vote was added.', [
         ':type' => $vote_type_id
       ]));
     }
-    else {
+    catch (AlreadyVotedException $e) {
       drupal_set_message(
-        t('You are not allowed to vote the same way multiple times.')
-        , 'warning'
+        t('You are not allowed to vote the same way multiple times.'),
+        'warning'
       );
     }
+
     $url = $request->getUriForPath($request->getPathInfo());
     return new RedirectResponse($url);
   }
