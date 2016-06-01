a5f5a934d713c4a841c080e8832fdc2d1e63e422
Edouard Cunibil <admin@duael.fr>
2016-06-01 17:16:38 +0200

    Improve like_and_dislike.vote route access.

--- src/LikeDislikeAccessController.php
+++ src/LikeDislikeAccessController.php
@@ -2,12 +2,14 @@
 
 namespace Drupal\like_and_dislike;
 
-use Drupal\Core\Access\AccessResultAllowed;
+use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
 
 class LikeDislikeAccessController extends ControllerBase {
 
-  public function vote() {
-    return AccessResultAllowed::allowed();
+  public function vote($entity_type_id, AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, "add or remove like votes on $entity_type_id");
   }
+
 }
